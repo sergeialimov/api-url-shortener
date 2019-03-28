5e7a6145d72dc7f2a2dd380db05597c9c9a27a84
Sergei Alimov <8912652@gmail.com>
2019-03-28 21:33:07 +0300

    add body-parser and dns

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+const dns = require('dns');
 
 var cors = require('cors');
 
@@ -16,6 +18,10 @@ var port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
 
@@ -25,12 +31,18 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.use(bodyParser.json());
 
+app.post("/api/shorturl/*", function (req, res) {
+  dns.lookup(req.body.url.split("/").pop(), (error, address, family) => {
+    console.error(error);
+    console.log(address);
+    console.log(family);
+  });
+  res.json({
+    original_url: req.body.url, "short_url": 1
+  });
+});
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
