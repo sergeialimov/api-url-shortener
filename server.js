a44397cf320472de74ca5679f920eade794e46b1
Sergei Alimov <8912652@gmail.com>
2019-04-01 19:31:53 +0300

    add redirect to freecodecamp

--- server.js
+++ server.js
@@ -15,16 +15,36 @@ var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
 // mongoose.connect(process.env.MONGOLAB_URI);
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
-
-// define schema
-const Schema = mongoose.Schema;
-
-// create instance of schema
-const websiteSchema = new Schema({
-  address : { type: String, required: true },
-  id : Number,
-})
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+// 
+// // define schema
+// const Schema = mongoose.Schema;
+// 
+// // create instance of schema
+// const websiteSchema = new Schema({
+//   address : { type: String, required: true },
+//   id : Number,
+// })
+// 
+// // create model from schema
+// const Website = mongoose.model('Website', websiteSchema);
+// 
+// // create document - an instance of model
+// const webInstance = new Person({
+//   name:'Arkadiy',
+//   age: 32,
+//   favoriteFoods: ['pelmeny', 'ryba'],
+// });
+// 
+// // save document
+// const createAndSaveWebsite = function(done) {
+//   webInstance.save(function(err, webInstance) {
+//      if (err) {
+//        console.log(err);
+//      }
+//      done(null, webInstance);
+//   });
+// };
 
 app.use(cors());
 
@@ -43,19 +63,28 @@ app.get('/', function(req, res){
 
 app.use(bodyParser.json());
 
-app.post("/api/shorturl/*", function (req, res) {
+app.post("/api/shorturl/*", function(req, res) {
   dns.lookup(req.body.url.split("/").pop(), (error, address, family) => {
     console.error(error);
     console.log(address);
     console.log(family);
     if (!address) {
-      res.json({"error":"invalid URL"});
+      res.json({ "error":"invalid URL" });
     }
   });
   res.json({
     original_url: req.body.url, "short_url": 1
   });
 });
+
+app.get("/api/shorturl/1", function(req, res) {
+// app.get("api/shourturl/:num?", function(req, res) {
+  // const num = req.params.num;
+  // const websites = {
+  //   1: 'https://www.freecodecamp.org/forum/',
+  // }
+  res.redirect('https://www.freecodecamp.org/forum/');
+})
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
