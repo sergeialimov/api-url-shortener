53a905cdb423552d90b5a216765b1a490b8cdc8a
Sergei Alimov <8912652@gmail.com>
2019-04-09 06:17:31 +0300

    refactoring

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 const uri = "mongodb+srv://8912652:good0101_@cluster0-bie1i.mongodb.net/urls?retryWrites=true";
 
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
+app.listen(port, function () {
+  console.log('Node.js listening at :3000...');
+});
+
 // mongoose.disconnect();
 mongoose.connect(uri, {
   keepAlive: true,
@@ -22,24 +32,14 @@ mongoose.connect(uri, {
   }
 );
 
-// define schema
-const Schema = mongoose.Schema;
-
 // create instance of schema
-const websiteSchema = new Schema({
+const websiteSchema = new mongoose.Schema({
+  // _id: Number,
   address: String,
-  test: Number,
-  id: Number,
 });
 
 // create model from schema
-const Websites = mongoose.model('Website', websiteSchema);
-
-app.use(cors());
-
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
+const Website = mongoose.model('websites', websiteSchema);
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -47,38 +47,25 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.use(bodyParser.json());
+app.post("/api/shorturl/new", async (request, response) => {
+  try {
+    var website = new Website({
+      address: request.body
+    });
+    var result = await website.save();
+    response.send(result);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
 
-app.post("/api/shorturl/new", async (req, res) => {
-  dns.lookup(req.body.url.split("/").pop(), (error, address, family) => {
-    if (!address) {
-      res.json({ "error": "invalid URL" });
+app.get("/api/shorturl/read", function(req, res) {
+  Website.find(function (err, product) {
+    if (err) {
+      return next(err);
+      res.send(product);
     }
   });
-
-  try {
-    var website = new Websites({
-      id: 1,
-      test: 1,
-      address: 'https://google.com',
-    });
-    console.log('=== 02 ---');
-    var result = website.save();
-    console.log('=== 03 ---');
-    // res.send(result);
-
-    res.json({
-      original_url: req.body.url,
-    });
-
-    console.log('=== 04 ---');
-  } catch (error) {
-    res.status(500).send(error);
-  };
-
-  // res.json({
-  //   original_url: req.body.url,
-  // });
 });
 
 app.get("/api/shorturl/:num?", function(req, res) {
@@ -88,11 +75,4 @@ app.get("/api/shorturl/:num?", function(req, res) {
     text: "succezz",
     // data: dbData,
   });
-  // res.redirect(websites[num]);
 });
-
-app.listen(port, function () {
-  console.log('Node.js listening ...');
-});
-
-mongoose.disconnect();
