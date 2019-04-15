17e26ec3d3a54afe6e251f9ab2f24f260027e139
Sergei Alimov <8912652@gmail.com>
2019-04-15 11:34:52 +0300

    add website.route.js

---
+++ routes/website.route.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const product_controller = require('../controllers/website.controller');
+
+
+// a simple test url to check that all of our files are communicating correctly.
+router.get('/test', website_controller.test);
+module.exports = router;
