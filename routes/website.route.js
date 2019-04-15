fa985fd54461ff2d90a77b6d049f08f23ca4f3e3
Sergei Alimov <8912652@gmail.com>
2019-04-15 17:03:14 +0300

    rename controller

--- routes/website.route.js
+++ routes/website.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const product_controller = require('../controllers/website.controller');
+const website_controller = require('../controllers/website.controller');
 
 
 // a simple test url to check that all of our files are communicating correctly.
