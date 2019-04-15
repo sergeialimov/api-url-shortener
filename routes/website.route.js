6b96eae9349fc1cadee5ab553ab98116ac8100ce
Sergei Alimov <8912652@gmail.com>
2019-04-15 18:08:50 +0300

    add default and shorturl/new to route

--- routes/website.route.js
+++ routes/website.route.js
@@ -6,4 +6,9 @@ const website_controller = require('../controllers/website.controller');
 
 // a simple test url to check that all of our files are communicating correctly.
 router.get('/test', website_controller.test);
+
+router.post('/api/shorturl/new', website_controller.website_new);
+
+router.get('/', website_controller.website_default);
+
 module.exports = router;
