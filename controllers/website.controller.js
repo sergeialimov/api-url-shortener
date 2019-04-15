1348752cda5bf2bec3f6a2cf559b741039180f43
Sergei Alimov <8912652@gmail.com>
2019-04-15 18:07:02 +0300

    add default page to controller

---
+++ controllers/website.controller.js
@@ -0,0 +1,33 @@
+const Website = require('../models/website.model');
+const dns = require('dns');
+
+//Simple version, without validation or sanitation
+exports.test = function (req, res) {
+    res.send('Greetings from the Test controller!');
+};
+
+exports.website_new = async (req, res, next) => {
+  dns.lookup(req.body.url.split("/").pop(), (error, address, family) => {
+    if (error) {
+      console.log(error);
+    }
+    if (!address) {
+      res.json({ "error": "invalid URL" });
+    }
+  });
+  try {
+    const website = new Website(req.body);
+    const result = await website.save();
+    const allWebsites = await Website.find();
+    res.send({
+      original_url: result.url,
+      short_url: allWebsites.length - 1,
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  };
+};
+
+exports.website_default = function(req, res){
+  res.sendFile(process.cwd() + '/views/index.html');
+};
