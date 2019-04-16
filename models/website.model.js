24433d99d181451a46a43c458e85aea2713c591a
Sergei Alimov <8912652@gmail.com>
2019-04-16 21:46:29 +0300

    add required: true

--- models/website.model.js
+++ models/website.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const WebsiteSchema = new mongoose.Schema({
-  _id: { type: Number, default: 0 },
+  _id: { type: Number, required: true, default: 0 },
   url: { type: String, required: true, max: 100 },
 });
 
