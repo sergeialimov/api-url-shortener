b4a9304077cfc132a7cc43c183dc05f030f57f63
Sergei Alimov <8912652@gmail.com>
2019-04-17 15:33:14 +0300

    move checking url to external function

--- controllers/website.controller.js
+++ controllers/website.controller.js
@@ -1,32 +1,39 @@
 const dns = require('dns');
+const util = require('util');
+
 const Website = require('../models/website.model');
 
+const dnsLookupAsync = util.promisify(dns.lookup);
+
+async function checkUrl (url) {
+  return dnsLookupAsync(url)
+    .then((obj) => obj.address)
+    .catch((err) => console.log(err));
+}
+
 exports.website_new = async (req, res) => {
-  dns.lookup(req.body.url.split('/')
-    .pop(), (error, address) => {
-    if (error) {
-      console.log(error);
+  const url = await checkUrl(req.body.url.split('/')
+    .pop());
+  if (!url) {
+    res.json({ error: `The URL ${req.body.url} is invalid` });
+  } else {
+    try {
+      const websites = await Website.find({ url: req.body.url });
+      const website = websites[0];
+      if (!website) {
+        const newWebsite = new Website(req.body);
+        const result = await newWebsite.save();
+        res.send({
+          original_url: result.url,
+          short_url: result._id,
+        });
+      } else {
+        res.send(`Specified url already existing\nThe shorturl is: ${website._id}`);
+      }
+    } catch (error) {
+      res.status(500)
+        .send(error);
     }
-    if (!address) {
-      res.json({ error: 'Invalid URL' });
-    }
-  });
-  try {
-    const websites = await Website.find({ url: req.body.url });
-    const website = websites[0];
-    if (!website) {
-      const newWebsite = new Website(req.body);
-      const result = await newWebsite.save();
-      res.send({
-        original_url: result.url,
-        short_url: result._id,
-      });
-    } else {
-      res.send(`Specified url already existing\nThe shorturl is: ${website._id}`);
-    }
-  } catch (error) {
-    res.status(500)
-      .send(error);
   }
 };
 
