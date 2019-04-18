475ce7432640e35be8c6271dfa5150924d3e858b
Sergei Alimov <8912652@gmail.com>
2019-04-18 13:25:53 +0300

    add getNextSequence

--- controllers/website.controller.js
+++ controllers/website.controller.js
@@ -2,6 +2,7 @@ const dns = require('dns');
 const util = require('util');
 
 const Website = require('../models/website.model');
+const Counter = require('../models/counter.model');
 
 const dnsLookupAsync = util.promisify(dns.lookup);
 
@@ -11,21 +12,43 @@ async function checkUrl (url) {
     .catch((err) => console.log(err));
 }
 
+const getNextSequence = async (name) => {
+  const res = Counter.findOneAndUpdate(
+    { _id: name },
+    { $inc: { seq: 1 } },
+    { new: true },
+    (err, response) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(response);
+      }
+    }
+  );
+  return res;
+};
+
 exports.website_new = async (req, res) => {
   const url = await checkUrl(req.body.url.split('/')
     .pop());
   if (!url) {
     res.json({ error: `The URL ${req.body.url} is invalid` });
+  // else should be replaced with return next()
   } else {
     try {
+      const counter = await getNextSequence('userid');
+      console.log('counter', counter.seq);
       const websites = await Website.find({ url: req.body.url });
       const website = websites[0];
       if (!website) {
-        const newWebsite = new Website(req.body);
+        const newWebsite = new Website({
+          _id: counter.seq,
+          url: req.body.url,
+        });
         const result = await newWebsite.save();
         res.send({
           original_url: result.url,
-          short_url: result._id,
+          short_url: counter.seq,
         });
       } else {
         res.send(`Specified url already existing\nThe shorturl is: ${website._id}`);
@@ -43,11 +66,13 @@ exports.website_default = (req, res) => {
 
 exports.website_open_short = (req, res, next) => {
   const { num } = req.params;
+  const parsedNum = parseInt(num, 10);
+  console.log(typeof parsedNum, '', parsedNum);
   // according to mongoose docs – arrow function should not be used
   /* eslint-disable prefer-arrow-callback */
   Website.find(function findWebsite (err, product) {
     if (!err) {
-      res.redirect(product[num].url);
+      res.redirect(product[parsedNum].url);
     }
     return next(err);
   });
