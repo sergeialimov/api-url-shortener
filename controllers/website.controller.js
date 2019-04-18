dc43d80dda8fded1376d7761cc16dadb606a4cd6
Sergei Alimov <8912652@gmail.com>
2019-04-18 22:22:48 +0300

    fix twiced incrementing

--- controllers/website.controller.js
+++ controllers/website.controller.js
@@ -14,7 +14,7 @@ async function checkUrl (url) {
 
 async function getNextSequence (name) {
   const result = await Counter.findOneAndUpdate(
-    { _id: name }, { $inc: { seq: 1 } }, { new: true }, ((err, data) => data)
+    { _id: name }, { $inc: { seq: 1 } }, { new: false }
   )
     .catch((err) => console.log(err));
   return result.seq;
